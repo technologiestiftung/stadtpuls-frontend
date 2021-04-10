a97cdd1658de428373bd809af318ac2fb29f64a1
Lucas Vogel <contact@vogelino.com>
2021-04-11 01:44:03 +0200

    feat(formtextinput): add FormTextInput component

---
+++ src/components/FormTextInput/FormTextInput.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { FormTextInput } from ".";
+
+describe("FormTextInput component", () => {
+  it("should render an input", () => {
+    render(<FormTextInput name='name' />);
+    const input = document.querySelector("input");
+    expect(input).toBeInTheDocument();
+  });
+  it("should not render a label when not provided", () => {
+    render(<FormTextInput name='name' />);
+    const label = document.querySelector("label");
+    expect(label).not.toBeInTheDocument();
+  });
+  it("should render a label when provided", () => {
+    render(<FormTextInput name='name' label='Name' />);
+    const label = screen.getByText(/Name/g);
+    expect(label).toBeInTheDocument();
+  });
+  it("should render an optional mention when provided", () => {
+    render(<FormTextInput name='name' label='Name' optional />);
+    const optional = screen.getByText(/Optional/g);
+    expect(optional).toBeInTheDocument();
+  });
+  it("should render errors when provided", () => {
+    render(<FormTextInput name='name' errors={["hey", "how"]} />);
+    const hey = screen.getByText(/hey/g);
+    expect(hey).toBeInTheDocument();
+    const how = screen.getByText(/how/g);
+    expect(how).toBeInTheDocument();
+  });
+});
---
+++ src/components/FormTextInput/index.tsx
@@ -0,0 +1,40 @@
+import { HTMLProps, forwardRef } from "react";
+
+interface FormTextInputPropType extends HTMLProps<HTMLInputElement> {
+  name: string;
+  label?: string;
+  errors?: string[];
+  optional?: boolean;
+}
+
+// eslint-disable-next-line react/display-name
+export const FormTextInput = forwardRef<
+  HTMLInputElement,
+  FormTextInputPropType
+>(({ name, label, optional = false, errors = [], ...inputProps }, ref) => (
+  <div className=' mb-2'>
+    {label && (
+      <label
+        htmlFor={name}
+        className='block mb-2 cursor-pointer transition hover:opacity-60'
+      >
+        {label}{" "}
+        {optional && (
+          <span className='text-gray-500 float-right text-sm transform translate-y-1'>
+            (Optional)
+          </span>
+        )}
+      </label>
+    )}
+    <input
+      {...inputProps}
+      ref={ref}
+      className={`mb-2 ${errors.length ? "error" : ""}`}
+    />
+    {errors.map(error => (
+      <p className='text-secondary text-sm' key={error}>
+        {error}
+      </p>
+    ))}
+  </div>
+));
