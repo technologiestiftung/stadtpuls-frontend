1cc3c583cf76ca9f40893279748caa54a37f5ff6
Lucas Vogel <contact@vogelino.com>
2021-04-11 12:02:40 +0200

    fix(textlink): fix ref forwarding in TextLink (required by next/link)

--- src/components/TextLink/index.tsx
+++ src/components/TextLink/index.tsx
@@ -1,14 +1,15 @@
-import { FC, HTMLProps } from "react";
+import { forwardRef, HTMLProps } from "react";
 
-export const TextLink: FC<HTMLProps<HTMLAnchorElement>> = ({
-  className = "",
-  children,
-  ...rest
-}) => (
+// eslint-disable-next-line react/display-name
+export const TextLink = forwardRef<
+  HTMLAnchorElement,
+  HTMLProps<HTMLAnchorElement>
+>(({ className = "", children, ...rest }, ref) => (
   <a
     {...rest}
+    ref={ref}
     className={`text-primary underline transition hover:opacity-60 cursor-pointer focus-offset ${className}`}
   >
     {children}
   </a>
-);
+));
