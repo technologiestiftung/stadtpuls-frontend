70e618a9fa0ee9317b2a875d5337b1c7c2bd5d11
Lucas Vogel <contact@vogelino.com>
2021-04-11 13:49:48 +0200

    test(SmallModal): Add test for SmallModal

---
+++ src/components/SmallModal/SmallModal.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { SmallModal } from ".";
+
+describe("SmallModal component", () => {
+  it("should render the provided title", () => {
+    render(<SmallModal title='Hello' />);
+    const title = screen.getByText(/Hello/g);
+    expect(title).toBeInTheDocument();
+  });
+  it("should render the provided chilren", () => {
+    render(<SmallModal title='Title'>Content</SmallModal>);
+    const content = screen.getByText(/Content/g);
+    expect(content).toBeInTheDocument();
+  });
+  it("should render the provided footerContent", () => {
+    render(<SmallModal title='Title' footerContent='Footer' />);
+    const footerContent = screen.getByText(/Footer/g);
+    expect(footerContent).toBeInTheDocument();
+  });
+});
