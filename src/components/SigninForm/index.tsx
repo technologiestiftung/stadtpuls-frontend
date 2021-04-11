c6998d4bf693996c1e7a32687a1e164536eb5f86
Lucas Vogel <contact@vogelino.com>
2021-04-11 11:58:44 +0200

    refactor(signinupformwrapper): use SignInUpFormWrapper in Signup and Signin forms

--- src/components/SigninForm/index.tsx
+++ src/components/SigninForm/index.tsx
@@ -1,10 +1,9 @@
 import { FC } from "react";
-import Link from "next/link";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, useForm } from "react-hook-form";
-import { TextLink } from "@components/TextLink";
 import { FormTextInput } from "@components/FormTextInput";
+import { SignInUpFormWrapper } from "@components/SignInUpFormWrapper";
 
 interface SigninFormData {
   email: string;
@@ -30,45 +29,21 @@ export const SigninForm: FC<{
   const onInternalSubmit = handleSubmit(data => onSubmit(data));
 
   return (
-    <form
-      onSubmit={onInternalSubmit}
-      className='max-w-md bg-white p-8 shadow-lg flex flex-col gap-8 place-content-between'
-      style={{ minHeight: 360 }}
-      noValidate
-    >
-      <fieldset>
-        <h1 className='text-primary text-3xl font-semibold m-0 mb-4'>Login</h1>
-        <Controller
-          name='email'
-          control={control}
-          defaultValue=''
-          render={({ field }) => (
-            <FormTextInput
-              {...field}
-              label='E-Mail'
-              placeholder='Deine E-Mail-Adresse...'
-              type='email'
-              errors={errors.email?.message ? [errors.email?.message] : []}
-            />
-          )}
-        />
-      </fieldset>
-      <fieldset className='flex gap-4 place-content-between'>
-        <section className='text-gray-500'>
-          Du hast noch keinen Account?{" "}
-          <Link href='/signup'>
-            <TextLink href='/signup' className='inline-block'>
-              Registriere dich
-            </TextLink>
-          </Link>
-        </section>
-        <section
-          className='flex flex-col'
-          style={{ justifyContent: "flex-end" }}
-        >
-          <input type='submit' value='Weiter' />
-        </section>
-      </fieldset>
-    </form>
+    <SignInUpFormWrapper onSubmit={onInternalSubmit} type='in'>
+      <Controller
+        name='email'
+        control={control}
+        defaultValue=''
+        render={({ field }) => (
+          <FormTextInput
+            {...field}
+            label='E-Mail'
+            placeholder='Deine E-Mail-Adresse...'
+            type='email'
+            errors={errors.email?.message ? [errors.email?.message] : []}
+          />
+        )}
+      />
+    </SignInUpFormWrapper>
   );
 };
--- src/components/SignupForm/index.tsx
+++ src/components/SignupForm/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
-import Link from "next/link";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Controller, useForm } from "react-hook-form";
 import { TextLink } from "@components/TextLink";
 import { FormTextInput } from "@components/FormTextInput";
 import { FormCheckbox } from "@components/FormCheckbox";
+import { SignInUpFormWrapper } from "@components/SignInUpFormWrapper";
 
 interface SignupFormData {
   email: string;
@@ -39,72 +39,45 @@ export const SignupForm: FC<{
     })
   );
 
+  const formatError = (errorMsg?: string): string[] =>
+    errorMsg ? [errorMsg] : [];
+
   return (
-    <form
-      onSubmit={onInternalSubmit}
-      className='max-w-md bg-white p-8 shadow-lg flex flex-col gap-8 place-content-between'
-      style={{ minHeight: 360 }}
-      noValidate
-    >
-      <fieldset>
-        <h1 className='text-primary text-3xl font-semibold m-0 mb-4'>
-          Registrierung
-        </h1>
-        <Controller
-          name='email'
-          control={control}
-          defaultValue=''
-          render={({ field }) => (
-            <FormTextInput
-              {...field}
-              label='E-Mail'
-              placeholder='Deine E-Mail-Adresse...'
-              type='email'
-              errors={errors.email?.message ? [errors.email?.message] : []}
-            />
-          )}
-        />
-        <Controller
-          name='areConditionsAccepted'
-          control={control}
-          defaultValue=''
-          render={({ field }) => (
-            <FormCheckbox
-              {...field}
-              label={
-                <>
-                  Ich akzeptiere die{" "}
-                  <TextLink href='https://www.technologiestiftung-berlin.de/de/datenschutz/'>
-                    Nutzungsbedingungen
-                  </TextLink>
-                  .
-                </>
-              }
-              errors={
-                errors.areConditionsAccepted?.message
-                  ? [errors.areConditionsAccepted?.message]
-                  : []
-              }
-            />
-          )}
-        />
-      </fieldset>
-      <fieldset className='flex gap-4 place-content-between'>
-        <section className='text-gray-500'>
-          Du hast bereits einen Account?{" "}
-          <Link href='/signin'>
-            <TextLink href='/signin' className='inline-block'>
-              Logge dich ein
-            </TextLink>
-          </Link>
-        </section>
-        <section
-          className='flex flex-col'
-          style={{ justifyContent: "flex-end" }}
-        >
-          <input type='submit' value='Weiter' />
-        </section>
-      </fieldset>
-    </form>
+    <SignInUpFormWrapper onSubmit={onInternalSubmit} type='up'>
+      <Controller
+        name='email'
+        control={control}
+        defaultValue=''
+        render={({ field }) => (
+          <FormTextInput
+            {...field}
+            label='E-Mail'
+            placeholder='Deine E-Mail-Adresse...'
+            type='email'
+            errors={formatError(errors.email?.message)}
+          />
+        )}
+      />
+      <Controller
+        name='areConditionsAccepted'
+        control={control}
+        defaultValue=''
+        render={({ field }) => (
+          <FormCheckbox
+            {...field}
+            label={
+              <>
+                Ich akzeptiere die{" "}
+                <TextLink href='https://www.technologiestiftung-berlin.de/de/datenschutz/'>
+                  Nutzungsbedingungen
+                </TextLink>
+                .
+              </>
+            }
+            errors={formatError(errors.areConditionsAccepted?.message)}
+          />
+        )}
+      />
+    </SignInUpFormWrapper>
   );
 };
