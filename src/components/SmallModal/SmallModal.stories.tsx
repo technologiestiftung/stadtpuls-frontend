94586feb50a0c0b06aa224fdd201044b22f1b7b8
Lucas Vogel <contact@vogelino.com>
2021-04-11 14:16:16 +0200

    chore(smallmodal): add real example to SmallModal

--- src/components/SmallModal/SmallModal.stories.tsx
+++ src/components/SmallModal/SmallModal.stories.tsx
@@ -1,5 +1,7 @@
 import { Story, Meta } from "@storybook/react";
 import { ReactNode } from "react";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { Button } from "@components/Button";
 import { SmallModal } from ".";
 
 export default {
@@ -19,13 +21,32 @@ const Template: Story<{
 
 export const WithoutFooterContent = Template.bind({});
 WithoutFooterContent.args = {
-  title: "I am a modal",
-  children: <h1>{"I'm a child"}</h1>,
+  title: "Gehe zu deinen E-Mails",
+  children: (
+    <p>
+      Wir haben dir eine E-Mail mir einem Anmeldungs-Link geschickt. Klicke den
+      Link an, um dich einzuloggen.
+    </p>
+  ),
 };
 
 export const WithFooterContent = Template.bind({});
 WithFooterContent.args = {
-  title: "I am a modal",
-  children: <h1>{"I'm a child"}</h1>,
-  footerContent: <h1>{"I'm a footer content"}</h1>,
+  title: "Willkommen!",
+  children: (
+    <div>
+      Du bist eingeloggt als:
+      <div className='mt-1'>
+        <span className='text-gray-300 mr-1'>
+          <AccountCircleIcon />
+        </span>
+        <span className='text-gray-400'>user@usermail.com</span>
+      </div>
+    </div>
+  ),
+  footerContent: (
+    <div className='block w-full text-right'>
+      <Button>Dein Profil</Button>
+    </div>
+  ),
 };
