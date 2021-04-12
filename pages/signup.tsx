9ac67363b4738647203bb2ab19b306c8db79a61c
Lucas Vogel <contact@vogelino.com>
2021-04-12 19:38:44 +0200

    feat(signup): add auth logic to signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,60 @@
-import { FC } from "react";
+import { FC, useState, useCallback } from "react";
+import { useRouter } from "next/router";
 import { SignupForm } from "@components/SignupForm";
+import { useAuth } from "@auth/Auth";
+import { SmallModal } from "@components/SmallModal";
+import { Button } from "@components/Button";
+
+const MagicLinkConfirmationModal: FC<{
+  onClose: () => void;
+}> = ({ onClose }) => (
+  <SmallModal
+    title='Gehe zu deinen E-Mails'
+    footerContent={
+      <div className='block w-full text-right'>
+        <Button onClick={onClose}>Alles klar</Button>
+      </div>
+    }
+  >
+    Wir haben dir eine E-Mail mir einem Anmeldungs-Link geschickt. Klicke den
+    Link an, um dich einzuloggen.
+  </SmallModal>
+);
+
+const SigningUpState: FC = () => (
+  <SmallModal title='Registrierung'>Sie werden registriert</SmallModal>
+);
 
 const SigninPage: FC = () => {
+  const [magicLinkWasSent, setMagicLinkWasSent] = useState<boolean>(false);
+  const [isSigningUp, setIsSigningUp] = useState<boolean>(false);
+  const { signIn } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = useCallback(
+    async (data: { email: string }): Promise<void> => {
+      setIsSigningUp(true);
+      const { error } = await signIn(data);
+
+      if (error) throw new Error(error.message);
+      setMagicLinkWasSent(true);
+      setIsSigningUp(false);
+    },
+    [signIn, setMagicLinkWasSent, setIsSigningUp]
+  );
+
   return (
     <div
       className='w-full h-full relative grid place-content-center'
       style={{ paddingTop: "10vmax" }}
     >
-      <SignupForm />
+      {isSigningUp && <SigningUpState />}
+      {!isSigningUp && magicLinkWasSent && (
+        <MagicLinkConfirmationModal onClose={() => router.push("/")} />
+      )}
+      {!isSigningUp && !magicLinkWasSent && (
+        <SignupForm onSubmit={handleSubmit} />
+      )}
     </div>
   );
 };
