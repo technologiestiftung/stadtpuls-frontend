8bece6f2da88047ca8ff4894f0e160c8c700ec38
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-12 08:43:44 +0200

    chore(Jest): mock CSS

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-	moduleNameMapper: {
-		'^@components(.*)$': '<rootDir>/src/components$1',
-		'^@lib(.*)$': '<rootDir>/src/lib$1',
-		'^@state(.*)$': '<rootDir>/src/state$1',
-		'^@common(.*)$': '<rootDir>/src/common$1',
-		'^@mocks(.*)$': '<rootDir>/src/mocks$1',
-	},
-}
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@lib(.*)$": "<rootDir>/src/lib$1",
+    "^@state(.*)$": "<rootDir>/src/state$1",
+    "^@common(.*)$": "<rootDir>/src/common$1",
+    "^@mocks(.*)$": "<rootDir>/src/mocks$1",
+    "\\.css$": "<rootDir>/src/mocks/cssMock.ts",
+  },
+};
