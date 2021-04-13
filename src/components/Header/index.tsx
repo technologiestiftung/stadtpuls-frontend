65ca27d25a946390bd772ed5e8b234825c137319
Lucas Vogel <contact@vogelino.com>
2021-04-13 15:45:05 +0200

    feat(header): add TSB logo and Anmeldung link in header (TBC)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,40 @@
-import { IotHubLogo } from "@components/IotHubLogo";
 import Link from "next/link";
-import React from "react";
+import { FC } from "react";
+import { useAuth } from "@auth/Auth";
+import { IotHubLogo } from "@components/IotHubLogo";
+import { TsbLogoSymbol } from "@components/TsbLogoSymbol";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+const LoginLink: FC = () => (
+  <Link href='/signin'>
+    <a href='/signin' className='group'>
+      <span className='text-gray-400 transform -translate-y-0.5 transition inline-block group-hover:text-primary'>
+        <AccountCircle />
+      </span>
+      <span className='inline-block ml-1 group-hover:text-primary transition'>
+        Anmeldung
+      </span>
+    </a>
+  </Link>
+);
 
 export const Header: React.FC = () => {
+  const { authenticatedUser } = useAuth();
   return (
     <header
       className={[
         "w-full p-4 bg-white shadow-lg z-50",
-        "relative",
+        "relative flex place-content-between",
         "md:sticky md:top-0",
       ].join(" ")}
     >
       <Link href='/'>
         <IotHubLogo />
       </Link>
+      <section className='flex gap-8'>
+        {authenticatedUser ? authenticatedUser.email : <LoginLink />}
+        <TsbLogoSymbol />
+      </section>
     </header>
   );
 };
