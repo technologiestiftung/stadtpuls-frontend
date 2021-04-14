b9d624a6df103f04329dddc4a0831cdd1268c40e
Lucas Vogel <contact@vogelino.com>
2021-04-14 13:36:35 +0200

    test(onclickoutsidehook): add test for onClickOutsideHook

--- src/lib/onClickOutsideHook/index.tsx
+++ src/lib/onClickOutsideHook/index.tsx
@@ -7,23 +7,23 @@ export default function useClickOutside<RefType extends HTMLElement>(
   const [isTouchEvent, setTouchEvent] = useState(false);
   const eventType = isTouchEvent ? "touchend" : "click";
 
-  function handleEvent(e: Event): void {
-    if (e.type === 'click' && isTouchEvent) { return; } // prettier-ignore
+  useEffect(() => {
+    function handleEvent(e: Event): void {
+      if (e.type === 'click' && isTouchEvent) { return; } // prettier-ignore
 
-    if (container.current && e.target !== null) {
-      if (!container.current.contains(e.target as Node)) {
-        callback(e);
+      if (container.current && e.target !== null) {
+        if (!container.current.contains(e.target as Node)) {
+          callback(e);
+        }
       }
     }
-  }
 
-  useEffect(() => {
     document.addEventListener(eventType, handleEvent, true);
 
     return () => {
       document.removeEventListener(eventType, handleEvent, true);
     };
-  });
+  }, [eventType, callback, isTouchEvent]);
 
   useEffect(() => {
     setTouchEvent("ontouchstart" in document.documentElement);
