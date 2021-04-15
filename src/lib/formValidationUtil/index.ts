0ddeee232dfa9afe822e06a23c22ab39c9ebdbb3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-15 16:21:54 +0200

    feat(DATAHUB-199): add validations

--- src/lib/formValidationUtil/index.ts
+++ src/lib/formValidationUtil/index.ts
@@ -4,3 +4,19 @@ export const requiredEmailValidation = yup
   .string()
   .email("Die angegebene E-Mail Adresse ist ungültig")
   .required("Sie müssen eine E-Mail Adresse angeben");
+
+export const requiredProjectTitleValidation = yup
+  .string()
+  .required("Das Projekt muss einen Titel haben");
+
+export const requiredProjectCategoryValidation = yup
+  .string()
+  .required("Eine Kategorie muss ausgewählt sein");
+
+export const requiredProjectDescriptionValidation = yup
+  .string()
+  .required(
+    "Das Projekt muss eine Beschreibung zwischen 10 und 140 Zeichen haben"
+  )
+  .min(10, "Die Beschreibung kann 10 bis 140 Zeichen enthalten")
+  .max(140, "Die Beschreibung kann 10 bis 140 Zeichen enthalten");
