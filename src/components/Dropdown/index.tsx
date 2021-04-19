43c3d0c6b866210d9247321cf658d4b6afe824a9
Lucas Vogel <contact@vogelino.com>
2021-04-19 10:06:24 +0200

    refactor(useclickoutside): rename onClickOutside to useClickOutside

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import useClickOutside from "@lib/onClickOutsideHook";
+import useClickOutside from "@lib/useClickOutside";
 import { FC, ReactNode, useState } from "react";
 
 interface DropdownPropType {
