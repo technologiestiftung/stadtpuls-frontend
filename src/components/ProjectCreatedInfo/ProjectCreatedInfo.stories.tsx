c536df313ae756b533b9e8038a898b07d4ac4114
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-19 23:05:27 +0200

    feat(ProjectCreatedInfo): create component

---
+++ src/components/ProjectCreatedInfo/ProjectCreatedInfo.stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from "@storybook/react";
+import { ProjectCreatedInfo, ProjectCreatedInfoType } from ".";
+
+export default {
+  title: "Layout/ProjectCreatedInfo",
+  component: ProjectCreatedInfo,
+} as Meta;
+
+const Template: Story<ProjectCreatedInfoType> = args => (
+  <ProjectCreatedInfo {...args}>{args.children}</ProjectCreatedInfo>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  projectTitle: "I'm a user controlled project title",
+  token: "1234-example-token-5678",
+  children: (
+    <>
+      <p>
+        {
+          "This is a next steps description. This is a next steps description. This is a next steps description. This is a next steps description. This is a next steps description. This is a next steps description."
+        }
+      </p>
+      <p>
+        {
+          "Possibly in two paragraphs. Possibly in two paragraphs. Possibly in two paragraphs. Possibly in two paragraphs. Possibly in two paragraphs. Possibly in two paragraphs."
+        }
+      </p>
+    </>
+  ),
+};
---
+++ src/components/ProjectCreatedInfo/index.tsx
@@ -0,0 +1,42 @@
+import { Anchor } from "@components/Button";
+import { FC, HTMLProps } from "react";
+import Link from "next/link";
+
+export interface ProjectCreatedInfoType extends HTMLProps<HTMLElement> {
+  projectTitle: string;
+  token: string;
+}
+
+export const ProjectCreatedInfo: FC<ProjectCreatedInfoType> = ({
+  projectTitle,
+  token,
+  children,
+  ...props
+}) => {
+  return (
+    <article className='bg-white max-w-prose' {...props}>
+      <h2 className='text-3xl text-blue-500 font-bold'>{projectTitle}</h2>
+      <p className='mt-4'>Dein Projekt wurde erfolgreich angelegt.</p>
+      <h3 className='mt-12 text-2xl text-blue-500 font-bold'>
+        Nächste Schritte
+      </h3>
+      <div className='mt-4'>{children}</div>
+      <h4 className='mt-12'>Token</h4>
+      <p className='mt-2 p-3 border border-gray-200 text-gray-500'>{token}</p>
+      <footer className='mt-24 flex justify-end'>
+        <Link href='/myprojects'>
+          <Anchor href='/myprojects'>Zur Projektübersicht</Anchor>
+        </Link>
+        <Anchor
+          href='https://console.thethingsnetwork.org/'
+          target='_blank'
+          rel='noopener noreferrer'
+          variant='primary'
+          className='ml-4'
+        >
+          Zur TTN-Konsole
+        </Anchor>
+      </footer>
+    </article>
+  );
+};
