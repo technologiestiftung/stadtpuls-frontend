44ffa72c2b5bcd01908a12497349d795142717a1
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-20 13:21:45 +0200

    feat(userProjects): create wrapper component
    With a sidebar for handling switching between projects

---
+++ src/components/UserProjectsWrapper/index.tsx
@@ -0,0 +1,54 @@
+import { FC, HTMLProps } from "react";
+import { useRouter } from "next/router";
+import { Anchor } from "@components/Button";
+import {
+  UserProjectsNav,
+  ProjectListItemType,
+} from "@components/UserProjectsNav";
+
+export interface UserProjectWrapperType extends HTMLProps<HTMLElement> {
+  projects: ProjectListItemType[];
+}
+
+export const UserProjectsWrapper: FC<UserProjectWrapperType> = ({
+  projects,
+  children,
+}) => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleSelectProject = (projectId: string): void => {
+    void router.push(`/account/projects/${projectId}`);
+  };
+
+  return (
+    <div
+      className='max-w-screen-lg mx-auto p-6 block md:grid gap-4 grid-cols-12'
+      style={{ paddingTop: "5vmax" }}
+    >
+      <section className='col-span-4'>
+        <h1 className='text-3xl text-blue-500 font-bold'>Meine Projekte</h1>
+        <div className='mt-8'>
+          <Anchor href={"/account/projects/new"} variant='primary'>
+            Neues Projekt
+          </Anchor>
+          <UserProjectsNav
+            projects={projects}
+            defaultSelectedProject={
+              projects.find(project => project.projectId === id) || projects[0]
+            }
+            onSelectProject={handleSelectProject}
+            className='mt-12'
+          />
+        </div>
+      </section>
+      <main
+        className={["md:col-span-8 mt-6 md:mt-0", "bg-white shadow-md"].join(
+          " "
+        )}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
