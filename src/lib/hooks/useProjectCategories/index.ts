21bc459fdc3020be915e250e4b0032e411942fbb
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-21 16:54:57 +0200

    fix(useProjectCategories): do not always recreate fetcher

--- src/lib/hooks/useProjectCategories/index.ts
+++ src/lib/hooks/useProjectCategories/index.ts
@@ -4,21 +4,21 @@ import { CategoriesType } from "@common/types/supabase";
 
 type CategoriesFetcherSignature = () => Promise<CategoriesType[] | null>;
 
+const categoriesFetcher: CategoriesFetcherSignature = async () => {
+  const { data: categories, error } = await supabase
+    .from<CategoriesType>("categories")
+    .select("*");
+
+  if (error) throw error;
+  else if (!categories) throw new Error("Categories not found");
+  return categories;
+};
+
 export const useProjectCategories = (): {
   isLoading: boolean;
   categories: CategoriesType[] | null;
   error: Error | null;
 } => {
-  const categoriesFetcher: CategoriesFetcherSignature = async () => {
-    const { data: categories, error } = await supabase
-      .from<CategoriesType>("categories")
-      .select("*");
-
-    if (error) throw error;
-    else if (!categories) throw new Error("Categories not found");
-    return categories;
-  };
-
   const { data, error } = useSWR<CategoriesType[] | null, Error>(
     "categories",
     categoriesFetcher
