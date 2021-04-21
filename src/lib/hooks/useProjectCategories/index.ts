b9d28465fbb7ccdeabab9e616c47d6d8f6571469
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-04-21 16:13:17 +0200

    feat(categories): provide hook
    And mock data

---
+++ src/lib/hooks/useProjectCategories/index.ts
@@ -0,0 +1,32 @@
+import useSWR from "swr";
+import { supabase } from "@auth/supabase";
+import { CategoriesType } from "@common/types/supabase";
+
+type CategoriesFetcherSignature = () => Promise<CategoriesType[] | null>;
+
+export const useProjectCategories = (): {
+  isLoading: boolean;
+  categories: CategoriesType[] | null;
+  error: Error | null;
+} => {
+  const categoriesFetcher: CategoriesFetcherSignature = async () => {
+    const { data: categories, error } = await supabase
+      .from<CategoriesType>("categories")
+      .select("*");
+
+    if (error) throw error;
+    else if (!categories) throw new Error("Categories not found");
+    return categories;
+  };
+
+  const { data, error } = useSWR<CategoriesType[] | null, Error>(
+    "categories",
+    categoriesFetcher
+  );
+
+  return {
+    isLoading: !error && !data,
+    categories: data || null,
+    error: error || null,
+  };
+};
---
+++ src/mocks/supabaseData/categories.ts
@@ -0,0 +1,17 @@
+export const fakeCategories = [
+  {
+    id: 1,
+    name: "Mock Category A",
+    description: "Description for A",
+  },
+  {
+    id: 1,
+    name: "Mock Category B",
+    description: "Description for B",
+  },
+  {
+    id: 1,
+    name: "Mock Category C",
+    description: "Description for C",
+  },
+];
