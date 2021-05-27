e93f9498eaafa0b2b616c45a845a3b64c9112385
Lucas Vogel <contact@vogelino.com>
2021-05-27 18:14:24 +0200

    feat(TableOfContents): Add scrollspy behavior to highlight active sections

--- src/components/TableOfContents/index.tsx
+++ src/components/TableOfContents/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 
 interface TableOfContentsLinkType {
   id: string;
@@ -21,14 +21,56 @@ const scrollToId = (id: string): void => {
     top: offsetPosition,
     behavior: "smooth",
   });
+};
 
-  const to = setTimeout(() => {
-    window.location.hash = id === "main-headline" ? "" : id;
-    clearTimeout(to);
-  }, 600);
+const getActiveLinkId = (): string => {
+  const h2Els = Array.from(document.querySelectorAll("h2"));
+  const mappedH2s = h2Els.map(h2El => ({
+    top: h2El.offsetTop,
+    id: h2El.getAttribute("id") || "main-headline",
+  }));
+  const windowEl = {
+    id: "window",
+    top: window.scrollY,
+  };
+  const allElements = [
+    {
+      id: "main-headline",
+      top: document.getElementById("main-headline")?.offsetTop || 0,
+    },
+    ...mappedH2s,
+    windowEl,
+  ].sort((a, b) => a.top - b.top);
+  const indexOfWindowScroll = allElements.findIndex(
+    ({ id }) => id === "window"
+  );
+  const indexOfPrevEl = Math.max(0, indexOfWindowScroll - 1);
+  const indexOfNextEl = Math.min(
+    allElements.length - 1,
+    indexOfWindowScroll + 1
+  );
+  const prevEl = allElements[indexOfPrevEl];
+  const nextEl = allElements[indexOfNextEl];
+  const activeEl = windowEl.top > nextEl.top - 200 ? nextEl : prevEl;
+  return activeEl.id === "window" ? prevEl.id : activeEl.id;
 };
 
 export const TableOfContents: FC<TableOfContentsPropType> = ({ links }) => {
+  const requestRef = useRef(0);
+  const [activeLinkId, setActiveLinkId] = useState<string>("main-headline");
+
+  const checkForActiveLinks = (): void => {
+    setActiveLinkId(getActiveLinkId());
+
+    requestRef.current = requestAnimationFrame(checkForActiveLinks);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(checkForActiveLinks);
+    return () => cancelAnimationFrame(requestRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <nav className='hidden fixed lg:block top-18 right-4 w-1/6 p-8'>
       <h4 className='font-bold mb-2 text-gray-500'>Inhalte</h4>
@@ -39,9 +81,12 @@ export const TableOfContents: FC<TableOfContentsPropType> = ({ links }) => {
               onClick={() => scrollToId(id)}
               className={[
                 "text-left cursor-pointer",
-                "text-gray-400 hover:text-blue-500 transition",
+                "hover:text-blue-500 transition",
                 "focus:ring-2 focus:ring-offset-gray-100 focus:ring-offset-2",
                 "focus:outline-none",
+                activeLinkId === id
+                  ? "font-bold text-blue-500"
+                  : "text-gray-400",
               ].join(" ")}
             >
               {text}
