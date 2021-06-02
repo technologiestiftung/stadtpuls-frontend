e6666e537796333ca2114d19601c105666b92e99
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-02 20:30:31 +0200

    feat(LineChart): visually test more data variants

--- src/components/LineChart/LineChart.stories.tsx
+++ src/components/LineChart/LineChart.stories.tsx
@@ -3,43 +3,118 @@ import { ThemeProvider } from "theme-ui";
 
 import theme from "../../style/theme";
 import { LineChart } from ".";
+import { LineGraphType } from "@common/interfaces";
 
 export default {
   title: "Charts/LineChart",
   component: LineChart,
 } as Meta;
 
-const Template: Story<{
-  width: number;
-  height: number;
-  data: Array<{
-    date: Date;
-    value: number;
-  }>;
-}> = ({ width, height, data }) => (
+const Template: Story<LineGraphType> = args => (
   <ThemeProvider theme={theme}>
-    <div className='bg-white' style={{ width, height }}>
-      <LineChart {...{ width, height, data }} />
+    <div
+      className='bg-white'
+      style={{ width: args.width, height: args.height }}
+    >
+      <LineChart {...args} />
     </div>
   </ThemeProvider>
 );
 
-export const Default = Template.bind({});
-Default.args = {
-  width: 400,
-  height: 200,
+export const TemperatureThroughoutDay = Template.bind({});
+TemperatureThroughoutDay.args = {
+  width: 600,
+  height: 300,
+  yAxisUnit: "°C",
+  xAxisUnit: "Messdatum",
   data: [
     {
-      date: new Date("2021-04-09T12:10:00.000Z"),
-      value: 10,
+      date: new Date("2021-04-09T04:13:55.000Z"),
+      value: 13.9,
     },
     {
-      date: new Date("2021-04-09T12:11:00.000Z"),
-      value: 20,
+      date: new Date("2021-04-09T08:43:01.000Z"),
+      value: 20.4,
     },
     {
-      date: new Date("2021-04-09T12:12:00.000Z"),
-      value: 15,
+      date: new Date("2021-04-09T13:21:39.000Z"),
+      value: 18.6,
+    },
+    {
+      date: new Date("2021-04-09T17:09:10.000Z"),
+      value: 20.9,
+    },
+    {
+      date: new Date("2021-04-09T20:43:59.000Z"),
+      value: 22.6,
+    },
+  ],
+};
+
+export const HumidityOnceADay = Template.bind({});
+HumidityOnceADay.args = {
+  width: 600,
+  height: 300,
+  yAxisUnit: "%",
+  xAxisUnit: "Messdatum",
+  data: [
+    {
+      date: new Date("2021-04-10T04:13:55.000Z"),
+      value: 70,
+    },
+    {
+      date: new Date("2021-04-11T04:13:55.000Z"),
+      value: 55,
+    },
+    {
+      date: new Date("2021-04-12T04:13:55.000Z"),
+      value: 61,
+    },
+    {
+      date: new Date("2021-04-13T04:13:55.000Z"),
+      value: 47,
+    },
+    {
+      date: new Date("2021-04-14T04:13:55.000Z"),
+      value: 68,
+    },
+    {
+      date: new Date("2021-04-15T04:13:55.000Z"),
+      value: 51,
+    },
+  ],
+};
+
+export const PaxcounterThroughoutDay = Template.bind({});
+PaxcounterThroughoutDay.args = {
+  width: 600,
+  height: 300,
+  yAxisUnit: "Personen",
+  xAxisUnit: "Messdatum",
+  data: [
+    {
+      date: new Date("2021-04-10T07:13:55.000Z"),
+      value: 49,
+    },
+    {
+      date: new Date("2021-04-11T08:13:55.000Z"),
+      value: 189,
+    },
+    {
+      date: new Date("2021-04-12T09:13:55.000Z"),
+      value: 790,
+    },
+    {
+      date: new Date("2021-04-13T17:13:55.000Z"),
+      value: 100,
+    },
+    {
+      date: new Date("2021-04-14T18:13:55.000Z"),
+      value: 1002,
+    },
+    {
+      date: new Date("2021-04-15T19:13:55.000Z"),
+      value: 306,
     },
   ],
 };
