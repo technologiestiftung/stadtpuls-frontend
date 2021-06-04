08dbd304d85a5ee13920ff56a3dcf0dd4e2fea04
Lucas Vogel <contact@vogelino.com>
2021-06-04 19:40:33 +0200

    fix(types): FIx types

--- src/__tests__/ProjectsOverviewPage.test.tsx
+++ src/__tests__/ProjectsOverviewPage.test.tsx
@@ -3,12 +3,18 @@ import { ThemeProvider } from "theme-ui";
 import theme from "../style/theme";
 
 import ProjectsOverviewPage from "../../pages/projects";
+import { fakeProjects } from "@components/LandingProjectsSlider/LandingProjectsSlider.stories";
 
 describe("home page", () => {
   it("should render without failing", () => {
     render(
       <ThemeProvider theme={theme}>
-        <ProjectsOverviewPage />
+        <ProjectsOverviewPage
+          projects={{
+            count: fakeProjects.length,
+            projects: fakeProjects,
+          }}
+        />
       </ThemeProvider>
     );
   });
