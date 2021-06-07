89fafbd6164aa95782342ce575b38c6c9a8f7c0f
Lucas Vogel <contact@vogelino.com>
2021-06-07 14:27:28 +0200

    fix(Accordion): Apply @lucasoeth's requested changes

--- src/components/Accordion/Accordion.test.tsx
+++ src/components/Accordion/Accordion.test.tsx
@@ -23,7 +23,7 @@ describe("Accordion component", () => {
     expect(three).toBeInTheDocument();
     expect(four).toBeInTheDocument();
   });
-  it("should should open an item if clicked", () => {
+  it("should open an item if clicked", () => {
     render(
       <Accordion
         items={[
@@ -36,16 +36,44 @@ describe("Accordion component", () => {
     );
 
     const content1 = screen.queryByText("Content-1");
-    expect(content1).toBeInTheDocument();
+    expect(content1).not.toBeInTheDocument();
     const content2 = screen.queryByText("Content-2");
     expect(content2).not.toBeInTheDocument();
+
+    const one = screen.getByText("One");
+    fireEvent.click(one);
+
+    const content1_2 = screen.queryByText("Content-1");
+    expect(content1_2).toBeInTheDocument();
+    const content2_2 = screen.queryByText("Content-2");
+    expect(content2_2).not.toBeInTheDocument();
 
     const two = screen.getByText("Two");
     fireEvent.click(two);
 
+    const content1_3 = screen.queryByText("Content-1");
+    expect(content1_3).not.toBeInTheDocument();
+    const content2_3 = screen.queryByText("Content-2");
+    expect(content2_3).toBeInTheDocument();
+  });
+
+  it("should close an item if already open", () => {
+    render(
+      <Accordion items={[{ id: "1", title: "One", content: "Content-1" }]} />
+    );
+
+    const content1 = screen.queryByText("Content-1");
+    expect(content1).not.toBeInTheDocument();
+
+    const one = screen.getByText("One");
+    fireEvent.click(one);
+
     const content1_2 = screen.queryByText("Content-1");
-    expect(content1_2).not.toBeInTheDocument();
-    const content2_2 = screen.queryByText("Content-2");
-    expect(content2_2).toBeInTheDocument();
+    expect(content1_2).toBeInTheDocument();
+
+    fireEvent.click(one);
+
+    const content1_3 = screen.queryByText("Content-1");
+    expect(content1_3).not.toBeInTheDocument();
   });
 });
