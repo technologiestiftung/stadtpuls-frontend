19ad1a90c025cca016e1a81abd39557ae6c3357a
Lucas Vogel <contact@vogelino.com>
2021-07-14 12:42:37 +0200

    feat(LandingProjectAbout): Adapt LandingProjectAbout to new ci

--- src/components/LandingProjectAbout/index.tsx
+++ src/components/LandingProjectAbout/index.tsx
@@ -1,14 +1,20 @@
 import { GitHub } from "@material-ui/icons";
 import { FC } from "react";
+import styles from "./LandingProjectAbout.module.css";
 
 const AboutTitle: FC = ({ children }) => (
   <h1
     className={[
       "text-xl sm:text-2xl md:text-3xl",
-      "text-blue font-semibold",
+      "text-green font-headline font-bold",
       "mt-6 mb-8",
     ].join(" ")}
   >
+    <img
+      src='/images/illustrations/microcontroller-2.svg'
+      alt='Microcontroller'
+      className='inline-block mr-4 align-top'
+    />
     {children}
   </h1>
 );
@@ -17,50 +23,18 @@ const techInfoColumns = [
   {
     title: "Backend",
     repo: "stadtpuls-api",
-    technologies: [
-      {
-        name: "Supabase",
-        description: "Open Source Firebase-like Service",
-        link: "http://supabase.io/",
-      },
-      {
-        name: "Fastify",
-        description: "Fast and low overhead web framework, for Node.js",
-        link: "https://www.fastify.io/",
-      },
-      {
-        name: "PostgreSQL Database",
-        description: "Open Source Relational Database",
-        link: "https://www.postgresql.org/",
-      },
-    ],
+    bgImage: "/images/icons/stadtpuls-api-technologies.svg",
   },
   {
     title: "Frontend",
     repo: "stadtpuls-frontend",
-    technologies: [
-      {
-        name: "Next.js",
-        description: "The React Framework for Production",
-        link: "https://nextjs.org/",
-      },
-      {
-        name: "Visx",
-        description:
-          "A collection of expressive, low-level visualization primitives for React",
-        link: "https://airbnb.io/visx/",
-      },
-      {
-        name: "SWR",
-        description: "React Hooks library for data fetching",
-        link: "https://swr.vercel.app/",
-      },
-    ],
+    bgImage: "/images/icons/stadtpuls-frontend-technologies.svg",
   },
 ];
 
 export const LandingProjectAbout: FC = () => (
-  <div className='bg-white'>
+  <div className='bg-black-dot-pattern relative mt-16'>
+    <div className={`${styles.image} absolute bg-cover w-1/2`} />
     <section
       className={[
         "container mx-auto max-w-8xl",
@@ -70,49 +44,41 @@ export const LandingProjectAbout: FC = () => (
     >
       <AboutTitle>Über das Projekt</AboutTitle>
       <div className='grid md:grid-cols-5 xl:grid-cols-2 gap-4 sm:gap-8'>
-        <div className='md:col-span-3 xl:col-span-1 prose max-w-none'>
-          <p className='max-w-none'>
-            Hier steht ein ausführlicher Beschreibungstext über Hintergründe des
-            Projekts, TSB, usw. Hier steht ein ausführlicher Beschreibungstext
-            über Hintergründe des Projekts, TSB, usw. Hier steht ein
-            ausführlicher Beschreibungstext über Hintergründe des Projekts, TSB,
-            usw. Hier steht ein ausführlicher Beschreibungstext über
-            Hintergründe des Projekts, TSB, usw. Hier steht ein ausführlicher
-            Beschreibungstext über Hintergründe des Projekts, TSB, usw.
-          </p>
-          <p className='max-w-none'>
-            Hier steht ein ausführlicher Beschreibungstext über Hintergründe des
-            Projekts, TSB, usw. Hier steht ein ausführlicher Beschreibungstext
-            über Hintergründe des Projekts, TSB, usw.
-          </p>
-        </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-1 xl:grid-cols-2 md:col-span-2 xl:col-span-1 gap-4 lg:gap-8'>
-          {techInfoColumns.map(({ repo, title, technologies }) => (
-            <div key={title} className=''>
-              <h4 className='text-xl font-bold mb-1'>{title}</h4>
-              <a
-                href={`https://github.com/technologiestiftung/${repo}`}
-                title={`Das Code-Repository von der Stadtpuls ${title}`}
-                className='text-sm text-gray-500 mb-4 block'
-              >
-                <GitHub className='mr-1 transform scale-75' />
+        <div className='md:col-span-3 xl:col-span-1'>
+          <div className='prose text-white max-w-none'>
+            <p className='max-w-none'>
+              Hier steht ein ausführlicher Beschreibungstext über Hintergründe
+              des Projekts, TSB, usw. Hier steht ein ausführlicher
+              Beschreibungstext über Hintergründe des Projekts, TSB, usw. Hier
+              steht ein ausführlicher Beschreibungstext über Hintergründe des
+              Projekts, TSB, usw. Hier steht ein ausführlicher Beschreibungstext
+              über Hintergründe des Projekts, TSB, usw. Hier steht ein
+              ausführlicher Beschreibungstext über Hintergründe des Projekts,
+              TSB, usw.
+            </p>
+          </div>
+          {techInfoColumns.map(({ repo, title, bgImage }) => (
+            <a
+              href={`https://github.com/technologiestiftung/${repo}`}
+              title={`Das Code-Repository von der Stadtpuls ${title}`}
+              key={title}
+              className={[
+                styles.repoLink,
+                "border border-blue shadow-blue",
+                "mt-8 p-8 block hover:animate-borderpulse-blue",
+                "bg-no-repeat",
+              ].join(" ")}
+              style={{ backgroundImage: `url(${bgImage})` }}
+            >
+              <h4 className='text-xl font-bold mb-1 text-green font-headline'>
+                {title}
+              </h4>
+              <p className='text-sm text-gray-500 block'>
+                <GitHub className='mr-1 transform scale-75 align-top inline-block' />
                 technologiestiftung/
-                <span className='text-black'>{repo}</span>
-              </a>
-              <ul>
-                {technologies.map(({ link, description, name }) => (
-                  <li key={name}>
-                    <a
-                      href={link}
-                      title={description}
-                      className='text-gray-500 underline hover:text-blue transition'
-                    >
-                      {name}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+                <span className='text-white'>{repo}</span>
+              </p>
+            </a>
           ))}
         </div>
       </div>
