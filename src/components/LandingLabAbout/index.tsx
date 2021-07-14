e462431df023d1854fbaa15e3c14addb64b0da4c
Lucas Vogel <contact@vogelino.com>
2021-07-14 14:22:15 +0200

    feat(LandingLabAbout): Adapt LandingLabAbout according to new ci

--- src/components/LandingLabAbout/index.tsx
+++ src/components/LandingLabAbout/index.tsx
@@ -1,13 +1,14 @@
 import useGithubUser from "@lib/hooks/useGithubUser";
 import { GitHub } from "@material-ui/icons";
 import { FC } from "react";
+import styles from "./LandingLabAbout.module.css";
 
 const AboutTitle: FC = ({ children }) => (
   <h1
     className={[
       "text-xl sm:text-2xl md:text-3xl",
-      "text-blue font-semibold",
-      "mt-6 mb-8",
+      "text-purple font-bold font-headline",
+      "mt-6 mb-1",
     ].join(" ")}
   >
     {children}
@@ -30,89 +31,90 @@ const furtherProjects = [
 export const LandingLabAbout: FC = () => {
   const { user } = useGithubUser("technologiestiftung");
   return (
-    <div
-      className='bg-white pb-40 sm:pb-56 lg:pb-96 bg-no-repeat'
-      style={{
-        backgroundImage: "url('/images/landing-lab-about.png')",
-        backgroundSize: "100% auto",
-        backgroundPosition: "center bottom",
-      }}
-    >
-      <section
-        className={[
-          "container mx-auto max-w-8xl",
-          "px-4 sm:px-6 md:px-8",
-          "py-8 sm:py-12 md:py-16",
-          "border-t border-gray-200",
-        ].join(" ")}
-      >
-        <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-2 gap-4 md:gap-8'>
-          <div className='sm:col-span-2 lg:col-span-1'>
-            <AboutTitle>
-              Wir bauen Prototypen
-              <br />
-              <span className='text-purple'>Für das Gemeinwohl</span>
-            </AboutTitle>
-            <div className='prose max-w-none'>
-              <p className='max-w-none'>
-                In der Technologiestiftung Berlin arbeiten Expert*innen aus den
-                Bereichen Frontend- und Backendentwicklung, Data Science,
-                Visualisierung, UI/UX zusammen, um aufzuzeigen, wie die
-                Gemeinschaft von Technologie profitieren kann. Oder so.
-              </p>
+    <div className='bg-white pb-4 sm:pb-8 lg:pb-12 bg-white-dot-pattern'>
+      <div className={`pt-4 sm:pt-8 lg:pt-12 ${styles.image}`}>
+        <section
+          className={[
+            "container mx-auto max-w-8xl",
+            "px-4 sm:px-6 md:px-8",
+            "py-8 sm:py-12 md:py-16",
+          ].join(" ")}
+        >
+          <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-2 gap-4 md:gap-8'>
+            <div className='sm:col-span-2 lg:col-span-1'>
+              <AboutTitle>Wir bauen Prototypen</AboutTitle>
+              <p className='text-lg mb-8'>Für das Gemeinwohl</p>
+              <div className='prose max-w-none'>
+                <p className='max-w-none'>
+                  In der Technologiestiftung Berlin arbeiten Expert*innen aus
+                  den Bereichen Frontend- und Backendentwicklung, Data Science,
+                  Visualisierung, UI/UX zusammen, um aufzuzeigen, wie die
+                  Gemeinschaft von Technologie profitieren kann. Oder so.
+                </p>
+              </div>
+            </div>
+            <div className='md:text-right'>
+              <img
+                src='/images/tsb-logo.svg'
+                alt='Logo der Technologiestiftung'
+                className='max-w-full h-16 mt-8 inline-block'
+              />
             </div>
           </div>
-          <div className='md:text-right'>
-            <img
-              src='/images/tsb-logo.svg'
-              alt='Logo der Technologiestiftung'
-              className='max-w-full h-16 mt-8 inline-block'
-            />
-          </div>
-        </div>
-        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-8 mt-12'>
-          <div
-            className={[
-              "text-2xl sm:text-3xl pb-4 pt-8 sm:pt-0 border-t",
-              "sm:border-t-0 border-gray-200",
-            ].join(" ")}
-          >
-            <strong className='font-bold'>{user?.public_repos || 182}</strong>{" "}
-            Open Source Projekte
-            <span className='block text-gray-500'>and counting</span>
-          </div>
-          <div className='xl:col-span-2 grid xl:grid-cols-2 gap-4 xl:gap-8'>
-            {furtherProjects.map(({ name, repo, logo }) => (
-              <a
-                key={name}
-                href={`https://github.com/technologiestiftung/${repo}`}
-                title={`Das Code-Repository von ${name}`}
-                target='_blank'
-                rel='noopener noreferrer'
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8 mt-12'>
+            <div>
+              <span
                 className={[
-                  "p-4 h-32 xl:h-48 rounded bg-white border border-gray-300",
-                  "gap-4 xl:gap-y-8 transition hover:border-blue hover:bg-gray-50",
-                  "flex flex-col justify-between max",
+                  "text-lg sm:text-xl bg-blue text-green gap-4",
+                  "font-headline font-bold flex place-items-center",
                 ].join(" ")}
               >
-                <div className='flex justify-between'>
-                  <h5 className='font-bold col-span-5 text-lg'>{name}</h5>
-                  <img
-                    src={`/images/repos/${logo}.svg`}
-                    alt={`Logo des "${name}" Projektes`}
-                    className='w-12'
-                  />
-                </div>
-                <div className='col-span-6 text-sm text-gray-500 flex'>
-                  <GitHub className='mr-1 transform scale-75' />
-                  technologiestiftung/
-                  <span className='text-black'>{repo}</span>
-                </div>
-              </a>
-            ))}
+                <strong className={["px-4 py-3 bg-green text-blue"].join(" ")}>
+                  {user?.public_repos || 182}
+                </strong>{" "}
+                <span className='w-full'>Open Source Projekte</span>
+                <strong
+                  className={["px-4 py-3 bg-purple text-white"].join(" ")}
+                >
+                  <GitHub />
+                </strong>
+              </span>
+            </div>
+            <div className='lg:col-span-2 grid lg:grid-cols-2 gap-4 xl:gap-8'>
+              {furtherProjects.map(({ name, repo, logo }) => (
+                <a
+                  key={name}
+                  href={`https://github.com/technologiestiftung/${repo}`}
+                  title={`Das Code-Repository von ${name}`}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={[
+                    "p-4 bg-white border border-gray-200",
+                    "gap-4 transition hover:border-blue hover:animate-borderpulse",
+                    "flex flex-col justify-between max shadow",
+                  ].join(" ")}
+                >
+                  <div className='flex justify-between'>
+                    <h5 className='font-bold font-headline col-span-5 text-lg'>
+                      {name}
+                    </h5>
+                    <img
+                      src={`/images/repos/${logo}.svg`}
+                      alt={`Logo des "${name}" Projektes`}
+                      className='w-12'
+                    />
+                  </div>
+                  <div className='col-span-6 text-sm text-gray-500 flex'>
+                    <GitHub className='mr-1 transform scale-75' />
+                    technologiestiftung/
+                    <span className='text-purple'>{repo}</span>
+                  </div>
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
