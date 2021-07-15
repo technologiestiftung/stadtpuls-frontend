df984ebe1587d397d2df2a08b1851f821a0ebec5
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-07-15 13:24:56 +0200

    style(DATAHUB-283): update LineChart and LinePath
    Plus: enable Tailwind JIT

--- src/components/LinePath/LinePath.test.tsx
+++ src/components/LinePath/LinePath.test.tsx
@@ -1,28 +1,24 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import { ThemeProvider } from "theme-ui";
 import { LinePath } from ".";
-import theme from "../../style/theme";
 
-describe("Footer component", () => {
+describe("LinePath component", () => {
   it("should render a path", () => {
     const width = 400;
     const height = 200;
     render(
-      <ThemeProvider theme={theme}>
-        <svg>
-          <LinePath
-            width={width}
-            height={height}
-            data={[
-              {
-                date: new Date().toISOString(),
-                value: 20,
-              },
-            ]}
-          />
-        </svg>
-      </ThemeProvider>
+      <svg>
+        <LinePath
+          width={width}
+          height={height}
+          data={[
+            {
+              date: new Date().toISOString(),
+              value: 20,
+            },
+          ]}
+        />
+      </svg>
     );
     const svgElement = document.querySelector("path");
     expect(svgElement).toBeInTheDocument();
