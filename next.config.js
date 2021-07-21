42b16192d4ba4713ed2d48f21dc2ebdc4e75e64a
Lucas Vogel <contact@vogelino.com>
2021-07-21 11:14:07 +0200

    feat(matomo): Add matomo and remove CookieBanner

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ module.exports = withMdxEnhanced({
               `frame-ancestors 'none'`,
               `worker-src 'self' blob:`,
               `child-src 'self' blob:`,
-              `connect-src 'self' ${process.env.NEXT_PUBLIC_TOKEN_API_URL} ${process.env.NEXT_PUBLIC_SUPABASE_URL} https://api.github.com https://api.mapbox.com https://events.mapbox.com`,
+              `connect-src 'self' ${process.env.NEXT_PUBLIC_TOKEN_API_URL} ${process.env.NEXT_PUBLIC_SUPABASE_URL}  ${process.env.NEXT_PUBLIC_MATOMO_URL} https://api.github.com https://api.mapbox.com https://events.mapbox.com`,
             ].join("; "),
           },
         ],
