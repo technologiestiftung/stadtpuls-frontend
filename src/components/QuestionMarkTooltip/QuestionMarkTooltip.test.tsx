4af431c678fb33df60049117f96823b55e2a0652
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-08-03 11:09:24 +0200

    test(DATAHUB-295): add missing props

--- src/components/QuestionMarkTooltip/QuestionMarkTooltip.test.tsx
+++ src/components/QuestionMarkTooltip/QuestionMarkTooltip.test.tsx
@@ -3,7 +3,13 @@ import { QuestionMarkTooltip } from ".";
 
 describe("QuestionMarkTooltip component", () => {
   it("should render its content", () => {
-    render(<QuestionMarkTooltip content='I am tooltip content' />);
+    render(
+      <QuestionMarkTooltip
+        id='test-id'
+        title='I am the title that the tooltip explains'
+        content='I am tooltip content'
+      />
+    );
     const content = screen.getByText(/I am tooltip content/i);
     expect(content).toBeInTheDocument();
   });
