06db939b486e4d96b95efe1da1ac154a1c8c23f8
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-09-13 15:01:38 +0200

    test(STADTPULS-385): update wording in createApiUrl utility

--- src/lib/requests/createApiUrl/createApiUrl.test.ts
+++ src/lib/requests/createApiUrl/createApiUrl.test.ts
@@ -1,11 +1,11 @@
 import { createApiUrl } from ".";
 describe("utility function createApiUrl", () => {
   it("should include the provided route", () => {
-    const url = createApiUrl("/projects");
-    expect(url.includes("/projects")).toBe(true);
+    const url = createApiUrl("/sensors");
+    expect(url.includes("/sensors")).toBe(true);
   });
   it("should not include undefined", () => {
-    const url = createApiUrl("/projects");
+    const url = createApiUrl("/sensors");
     expect(url.includes("undefined")).toBe(false);
   });
 });
