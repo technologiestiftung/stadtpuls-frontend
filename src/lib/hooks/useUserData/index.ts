611035ea98e3b1739e79256cc7c93dfdee12dea6
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-09-20 16:40:55 +0200

    refactor(STADTPULS-385): update useUserdata hook

--- src/lib/hooks/useUserData/index.ts
+++ src/lib/hooks/useUserData/index.ts
@@ -1,39 +1,27 @@
-import {
-  deleteProjectsProject,
-  updateProjectsProject,
-} from "./updateProjectsProject";
 import useSWR, { mutate } from "swr";
 import { supabase } from "@auth/supabase";
-import {
-  UserProfilesType,
-  AuthenticatedUsersType,
-  ProjectsType,
-  DevicesType,
-} from "@common/types/supabase_DEPRECATED";
+import { AuthenticatedUsersType } from "@common/types/supabase_DEPRECATED";
 import { useAuth } from "@auth/Auth";
 import {
-  addProjectsDevice,
-  deleteProjectsDevice,
-  updateProjectsDevice,
+  createSensorLocally,
+  deleteSensorLocally,
+  updateSensorsLocally,
 } from "./updateProjectsDevice";
 import { useState } from "react";
+import { definitions } from "@common/types/supabase";
+import { sensorQueryString } from "../usePublicSensors";
 
 type UserFetcherSignature = (
   userId?: AuthenticatedUsersType["id"],
   isLoadingAuth?: boolean
-) => Promise<UserProfilesType | null>;
-
-type ProjectsFetcherSignature = (
-  userId?: AuthenticatedUsersType["id"],
-  isLoadingAuth?: boolean
-) => Promise<ProjectsType[] | null>;
+) => Promise<definitions["user_profiles"] | null>;
 
 const fetchUser: UserFetcherSignature = async (userId, isLoadingAuth) => {
   if (isLoadingAuth || isLoadingAuth === undefined) return null;
   if (!userId) throw new Error("Not authenticated");
 
   const { data: user, error } = await supabase
-    .from<UserProfilesType>("userprofiles")
+    .from<definitions["user_profiles"]>("user_profiles")
     .select("name")
     .eq("id", userId)
     .single();
@@ -44,7 +32,12 @@ const fetchUser: UserFetcherSignature = async (userId, isLoadingAuth) => {
   return user;
 };
 
-const fetchProjects: ProjectsFetcherSignature = async (
+type SensorsFetcherSignature = (
+  userId?: AuthenticatedUsersType["id"],
+  isLoadingAuth?: boolean
+) => Promise<definitions["sensors"][] | null>;
+
+const fetchUserSensors: SensorsFetcherSignature = async (
   userId,
   isLoadingAuth
 ) => {
@@ -52,124 +45,78 @@ const fetchProjects: ProjectsFetcherSignature = async (
   if (!userId) throw new Error("Not authenticated");
 
   const { data, error } = await supabase
-    .from<ProjectsType>("projects")
-    .select(
-      `
-      id,
-      name,
-      description,
-      connectype,
-      location,
-      category: categoryId (
-        id,
-        name,
-        description
-      ),
-      devices (
-        id,
-        externalId,
-        name,
-        records (
-          id,
-          recordedAt
-        )
-      )
-    `
-    )
-    .eq("userId", userId);
+    .from<definitions["sensors"]>("sensors")
+    .select(sensorQueryString)
+    .eq("user_id", userId);
 
   if (error) throw error;
   else if (!data)
-    throw new Error(`Projects for user with id "${userId} was not found"`);
+    throw new Error(`Sensors for user with id "${userId} not found"`);
   return data;
 };
 
-const addDevice = async (device: Omit<DevicesType, "id">): Promise<void> => {
-  if (!device.userId) throw new Error("Not authenticated");
-
-  const { error } = await supabase
-    .from<DevicesType>("devices")
-    .insert([{ ...device, id: undefined }]);
-
-  if (error) throw error;
-};
-
-const updateDevice = async ({
-  id,
-  userId,
-  externalId,
-  name,
-}: Partial<DevicesType>): Promise<void> => {
-  if (!userId) throw new Error("Not authenticated");
-
-  const { error } = await supabase
-    .from<DevicesType>("devices")
-    .update({ externalId, name })
-    .eq("id", id)
-    .eq("userId", userId);
-
-  if (error) throw error;
-};
-
-const deleteDevice = async (
-  id: DevicesType["id"],
-  userId: string | undefined
+const createSensor = async (
+  sensor: Omit<definitions["sensors"], "id">
 ): Promise<void> => {
-  if (!userId) throw new Error("Not authenticated");
+  if (!sensor.user_id) throw new Error("Not authenticated");
 
   const { error } = await supabase
-    .from<DevicesType>("devices")
-    .delete()
-    .eq("id", id)
-    .eq("userId", userId);
+    .from<definitions["sensors"]>("sensors")
+    .insert([{ ...sensor, id: undefined }]);
 
   if (error) throw error;
 };
 
-const addProject = async (
-  project: Omit<ProjectsType, "id" | "createdAt">
-): Promise<ProjectsType[] | null> => {
-  if (!project.userId) throw new Error("Not authenticated");
-
-  const { error, data } = await supabase
-    .from<ProjectsType>("projects")
-    .insert([{ ...project }]);
-
-  if (error) throw error;
-  return data;
-};
-
-const updateProject = async ({
+const updateSensor = async ({
   id,
+  created_at,
   name,
+  user_id,
+  external_id,
+  latitude,
+  longitude,
+  altitude,
+  connection_type,
+  category_id,
+  icon_id,
   description,
-  connectype,
   location,
-  userId,
-  categoryId,
-}: Partial<ProjectsType>): Promise<void> => {
-  if (!userId) throw new Error("Not authenticated");
+}: Partial<definitions["sensors"]>): Promise<void> => {
+  if (!user_id) throw new Error("Not authenticated");
 
   const { error } = await supabase
-    .from<ProjectsType>("projects")
-    .update({ name, description, connectype, location, categoryId })
+    .from<definitions["sensors"]>("sensors")
+    .update({
+      id,
+      created_at,
+      name,
+      description,
+      external_id,
+      location,
+      latitude,
+      longitude,
+      altitude,
+      connection_type,
+      category_id,
+      icon_id,
+    })
     .eq("id", id)
-    .eq("userId", userId);
+    .eq("user_id", user_id);
 
   if (error) throw error;
 };
 
-const deleteProject = async (
-  id: number,
-  userId: string | undefined
+const deleteSensor = async (
+  id: definitions["sensors"]["id"],
+  user_id: string | undefined
 ): Promise<void> => {
-  if (!userId) throw new Error("Not authenticated");
+  if (!user_id) throw new Error("Not authenticated");
 
   const { error } = await supabase
-    .from<ProjectsType>("projects")
+    .from<definitions["sensors"]>("sensors")
     .delete()
     .eq("id", id)
-    .eq("userId", userId);
+    .eq("user_id", user_id);
 
   if (error) throw error;
 };
@@ -181,14 +128,15 @@ const updateUser = async (
   if (!userId) throw new Error("Not authenticated");
 
   const nameReset = await supabase
-    .from<UserProfilesType>("userprofiles")
+    .from<definitions["user_profiles"]>("user_profiles")
     .update({ name: newName })
     .eq("id", userId);
 
   if (nameReset.error) throw nameReset.error;
 };
 
-const updateEmail = async (
+// We currently don't support updating the email. TODO: potentially delete this function
+/* const updateEmail = async (
   newEmail: string,
   userId: string | undefined
 ): Promise<void> => {
@@ -199,7 +147,7 @@ const updateEmail = async (
   });
 
   if (error) throw error;
-};
+}; */
 
 const deleteUser = async (userId: string | undefined): Promise<void> => {
   if (!userId) throw new Error("Not authenticated");
@@ -212,19 +160,14 @@ const deleteUser = async (userId: string | undefined): Promise<void> => {
 export const useUserData = (): {
   isLoading: boolean;
   authenticatedUser: AuthenticatedUsersType | null;
-  user: UserProfilesType | null;
-  projects: ProjectsType[] | null;
+  user: definitions["user_profiles"] | null;
+  sensors: definitions["sensors"][] | null;
   error: Error | null;
-  addDevice: (device: Omit<DevicesType, "id">) => Promise<void>;
-  updateDevice: (device: Omit<DevicesType, "projectId">) => Promise<void>;
-  deleteDevice: (id: number) => Promise<void>;
-  addProject: (
-    project: Omit<ProjectsType, "id" | "createdAt">
-  ) => Promise<ProjectsType[] | null | void>;
-  updateProject: (project: Omit<ProjectsType, "createdAt">) => Promise<void>;
-  deleteProject: (id: number) => Promise<void>;
+  createSensor: (sensor: Omit<definitions["sensors"], "id">) => Promise<void>;
+  updateSensor: (sensor: Partial<definitions["sensors"]>) => Promise<void>;
+  deleteSensor: (id: number) => Promise<void>;
   updateUser: (newName: string) => Promise<void>;
-  updateEmail: (newEmail: string) => Promise<void>;
+  /* updateEmail: (newEmail: string) => Promise<void>; */
   deleteUser: () => Promise<void>;
 } => {
   const [actionError, setActionError] = useState<Error | null>(null);
@@ -232,85 +175,51 @@ export const useUserData = (): {
   const userId = authenticatedUser?.id;
 
   const userParams = ["userData", userId, isLoadingAuth];
-  const user = useSWR<UserProfilesType | null, Error>(userParams, () =>
-    fetchUser(userId, isLoadingAuth)
+  const user = useSWR<definitions["user_profiles"] | null, Error>(
+    userParams,
+    () => fetchUser(userId, isLoadingAuth)
   );
 
-  const projectsParams = ["projects", userId, isLoadingAuth];
-  const projects = useSWR<ProjectsType[] | null, Error>(projectsParams, () =>
-    fetchProjects(userId, isLoadingAuth)
+  const sensorsParams = ["sensors", userId, isLoadingAuth];
+  const sensors = useSWR<definitions["sensors"][] | null, Error>(
+    sensorsParams,
+    () => fetchUserSensors(userId, isLoadingAuth)
   );
 
   return {
-    isLoading: !user.error && !projects.error && !user.data && !projects.data,
+    isLoading: !user.error && !user.data,
     authenticatedUser: authenticatedUser || null,
     user: user.data || null,
-    projects: projects.data || null,
-    error: projects.error || user.error || actionError || null,
-    addDevice: async device => {
-      if (!projects.data || projects.error) return;
+    sensors: sensors.data || null,
+    error: user.error || actionError || null,
+    createSensor: async sensor => {
+      if (!sensors.data || sensors.error) return;
       setActionError(null);
       void mutate(
-        projectsParams,
-        addProjectsDevice(projects.data, device),
+        sensorsParams,
+        createSensorLocally(sensors.data, sensor),
         false
       );
-      await addDevice({ ...device, userId }).catch(setActionError);
-      void mutate(projectsParams);
+      await createSensor(sensor).catch(setActionError);
+      void mutate(sensorsParams);
     },
-    updateDevice: async device => {
-      if (!projects.data || projects.error) return;
+    updateSensor: async sensor => {
+      if (!sensors.data || sensors.error) return;
       setActionError(null);
       void mutate(
-        projectsParams,
-        updateProjectsDevice(projects.data, device),
+        sensorsParams,
+        updateSensorsLocally(sensors.data, sensor as definitions["sensors"]),
         false
       );
-      await updateDevice({ ...device, userId }).catch(setActionError);
-      void mutate(projectsParams);
+      await updateSensor(sensor).catch(setActionError);
+      void mutate(sensorsParams);
     },
-    deleteDevice: async id => {
-      if (!projects.data || projects.error) return;
+    deleteSensor: async id => {
+      if (!sensors.data || sensors.error) return;
       setActionError(null);
-      void mutate(
-        projectsParams,
-        deleteProjectsDevice(projects.data, id),
-        false
-      );
-      await deleteDevice(id, userId).catch(setActionError);
-      void mutate(projectsParams);
-    },
-    addProject: async project => {
-      if (!projects.data || projects.error) return;
-      setActionError(null);
-      void mutate(projectsParams, [...projects.data, project], false);
-      const newProjects = await addProject({ ...project, userId }).catch(
-        setActionError
-      );
-      void mutate(projectsParams);
-      return newProjects;
-    },
-    updateProject: async project => {
-      if (!projects.data || projects.error) return;
-      setActionError(null);
-      void mutate(
-        projectsParams,
-        updateProjectsProject(projects.data, project as ProjectsType),
-        false
-      );
-      await updateProject({ ...project, userId }).catch(setActionError);
-      void mutate(projectsParams);
-    },
-    deleteProject: async id => {
-      if (!projects.data || projects.error) return;
-      setActionError(null);
-      void mutate(
-        projectsParams,
-        deleteProjectsProject(projects.data, id),
-        false
-      );
-      await deleteProject(id, userId).catch(setActionError);
-      void mutate(projectsParams);
+      void mutate(sensorsParams, deleteSensorLocally(sensors.data, id), false);
+      await deleteSensor(id, userId).catch(setActionError);
+      void mutate(sensorsParams);
     },
     updateUser: async name => {
       if (user.data?.name === name) return;
@@ -318,12 +227,12 @@ export const useUserData = (): {
       await updateUser(name, userId).catch(setActionError);
       void mutate(userParams);
     },
-    updateEmail: async email => {
+    /* updateEmail: async email => {
       if (authenticatedUser?.email === email) return;
       void mutate(userParams, { ...authenticatedUser, email }, false);
       await updateEmail(email, userId).catch(setActionError);
       void mutate(userParams);
-    },
+    }, */
     deleteUser: async () => {
       void mutate(userParams, null, false);
       await deleteUser(userId).catch(setActionError);
