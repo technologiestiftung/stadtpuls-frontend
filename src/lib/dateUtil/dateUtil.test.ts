5dde74eec5839e3792370f18f09b72c2c5e145b3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-09-21 16:04:57 +0200

    fix: remove wrongly used sensor_id

--- src/lib/dateUtil/dateUtil.test.ts
+++ src/lib/dateUtil/dateUtil.test.ts
@@ -5,7 +5,6 @@ describe("createDateValueArray", () => {
     const dateValueArray = createDateValueArray([
       {
         id: 1,
-        sensor_id: 1,
         recorded_at: "2021-04-08T20:32:49.796Z",
         measurements: [20],
       },
@@ -16,7 +15,6 @@ describe("createDateValueArray", () => {
     const dateValueArray = createDateValueArray([
       {
         id: 1,
-        sensor_id: 1,
         recorded_at: "2021-04-08T20:32:49.796Z",
         measurements: [20],
       },
@@ -29,19 +27,16 @@ describe("createDateValueArray", () => {
     const dateValueArray = createDateValueArray([
       {
         id: 2,
-        sensor_id: 1,
         recorded_at: "2021-02-08T20:32:49.796Z",
         measurements: [2],
       },
       {
         id: 3,
-        sensor_id: 1,
         recorded_at: "2021-03-08T20:32:49.796Z",
         measurements: [3],
       },
       {
         id: 1,
-        sensor_id: 1,
         recorded_at: "2021-01-08T20:32:49.796Z",
         measurements: [1],
       },
