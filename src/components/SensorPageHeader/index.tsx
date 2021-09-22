47ac877f82823af5946384eda379ac3339b067cc
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-09-22 12:57:56 +0200

    fix: not yet update path

--- src/components/SensorPageHeader/index.tsx
+++ src/components/SensorPageHeader/index.tsx
@@ -5,7 +5,7 @@ import React, { FC } from "react";
 import Link from "next/link";
 import { SensorSymbol } from "@components/SensorSymbol";
 import { CopyTextField } from "@components/CopyTextField";
-import { ProjectPreviewMap } from "@components/ProjectPreviewMap";
+import { SensorPreviewMap } from "@components/SensorPreviewMap";
 import { Button } from "@components/Button";
 
 export interface SensorPageHeaderPropType {
@@ -34,7 +34,7 @@ const MapBackground: FC<Pick<SensorPageHeaderPropType, "geocoordinates">> = ({
   >
     <div className='bg-white hidden md:block' />
     <div className='relative'>
-      <ProjectPreviewMap
+      <SensorPreviewMap
         mapWidth='100%'
         mapHeight='100%'
         viewport={geocoordinates}
