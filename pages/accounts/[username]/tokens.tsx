1d4b0b125028759c9e73c363aea7b5fac09ca882
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-09-28 12:37:33 +0200

    feat(STADTPULS-355): add user flow for tokens page
    Currently with mock data

--- pages/accounts/[username]/tokens.tsx
+++ pages/accounts/[username]/tokens.tsx
@@ -1,11 +1,39 @@
 import { getAccountDataByUsername } from "@lib/requests/getAccountDataByUsername";
 import { ParsedAccountType } from "@lib/hooks/usePublicAccounts";
 import { GetServerSideProps } from "next";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { UserInfoWithData } from "@components/UserInfoHeader/withData";
 import { useUserData } from "@lib/hooks/useUserData";
 import { TextLink } from "@components/TextLink";
 import { Alert } from "@components/Alert";
+import { TokenForm } from "@components/TokenForm";
+import { TokenItem } from "@components/TokenItem";
+import { definitions } from "@common/types/supabase";
+import { TokenCreationModal } from "@components/TokenCreationModal";
+import { TokenDeletionModal } from "@components/TokenDeletionModal";
+
+type TokenType = Omit<definitions["auth_tokens"], "id">;
+
+const tokens: TokenType[] = [
+  {
+    description: "My first token",
+    nice_id: 19,
+    scope: "sudo",
+    user_id: "my-user-id",
+  },
+  {
+    description: "Another token",
+    nice_id: 55,
+    scope: "sudo",
+    user_id: "my-user-id",
+  },
+  {
+    description: "Token Token Token",
+    nice_id: 329,
+    scope: "sudo",
+    user_id: "my-user-id",
+  },
+];
 
 export const getServerSideProps: GetServerSideProps = async context => {
   try {
@@ -32,6 +60,12 @@ const AccountTokensPage: FC<AccountTokensPagePropType> = ({ routeAccount }) => {
       : routeAccount;
   const isOwnerAndLoggedIn =
     isLoggedIn && loggedInAccount?.username === routeAccount.username;
+
+  const [creationModalIsOpen, setCreationModalIsOpen] = useState(false);
+  const [deletionModalIsOpen, setDeletionModalIsOpen] = useState(false);
+  const [touchedToken, setTouchedToken] = useState<TokenType | undefined>(
+    undefined
+  );
   return (
     <>
       <UserInfoWithData routeAccount={account} activeTab='tokens' />
@@ -52,6 +86,66 @@ const AccountTokensPage: FC<AccountTokensPagePropType> = ({ routeAccount }) => {
             }
           />
         </div>
+      )}
+      {isOwnerAndLoggedIn && (
+        <div className='container max-w-8xl mx-auto px-4 py-16'>
+          <TokenForm
+            onSubmit={() => {
+              console.log("Creating token");
+              // TODO: create new token
+              setCreationModalIsOpen(true);
+            }}
+          />
+          {tokens.length === 0 && (
+            <div className='text-gray-500 text-center mt-12'>
+              Noch keine Tokens vorhanden.
+            </div>
+          )}
+          {tokens &&
+            tokens.length > 0 &&
+            tokens.map(token => {
+              return (
+                <TokenItem
+                  key={`token-${token.nice_id}`}
+                  name={token.description}
+                  onRegenerate={() => {
+                    console.log("Regenerating old token:", touchedToken);
+                    setTouchedToken(token);
+                    // TODO: delete old token and create new token
+                    setCreationModalIsOpen(true);
+                  }}
+                  onInitiateDelete={() => {
+                    console.log("Token about to be deleted");
+                    setTouchedToken(token);
+                    setDeletionModalIsOpen(true);
+                  }}
+                />
+              );
+            })}
+        </div>
+      )}
+      {creationModalIsOpen && (
+        <TokenCreationModal
+          tokenDescription='My newly generated token'
+          token='1234567'
+          onClose={() => setCreationModalIsOpen(false)}
+        />
+      )}
+      {deletionModalIsOpen && touchedToken && (
+        <TokenDeletionModal
+          tokenDescription={touchedToken.description}
+          onDelete={() => {
+            console.log("Deleting token:", touchedToken);
+
+            // TODO: delete token
+            setTouchedToken(undefined);
+            setDeletionModalIsOpen(false);
+          }}
+          onCancel={() => {
+            setTouchedToken(undefined);
+            setDeletionModalIsOpen(false);
+          }}
+        />
       )}
     </>
   );
