2247e170082829c4265ceb1bcbe3a108524642d4
Lucas Vogel <contact@vogelino.com>
2021-09-29 18:02:51 +0200

    test(createSigningApiUrl): Fix types in test for createSigningApiUrl

--- src/lib/requests/createSigningApiUrl/createSigningApiUrl.test.ts
+++ src/lib/requests/createSigningApiUrl/createSigningApiUrl.test.ts
@@ -1,11 +1,11 @@
 import { createSigningApiUrl } from ".";
 describe("utility function createSigningApiUrl", () => {
   it("should include the provided route", () => {
-    const url = createSigningApiUrl("/signin");
+    const url = createSigningApiUrl("signin");
     expect(url.includes("/signin")).toBe(true);
   });
   it("should not include undefined", () => {
-    const url = createSigningApiUrl("/sensors");
+    const url = createSigningApiUrl("signup");
     expect(url.includes("undefined")).toBe(false);
   });
 });
