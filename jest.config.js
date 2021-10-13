20a7768a96953149f4b206cf86134780db690381
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-10-13 17:50:16 +0200

    test: explicitly set browser testing environment
    Because since v27.0.0, Jest defaults to Node as the test environment

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-	moduleNameMapper: {
-		'^@components(.*)$': '<rootDir>/src/components$1',
-		'^@lib(.*)$': '<rootDir>/src/lib$1',
-		'^@common(.*)$': '<rootDir>/src/common$1',
-		'^@mocks(.*)$': '<rootDir>/src/mocks$1',
-		'^@auth(.*)$': '<rootDir>/src/auth$1',
-		'\\.css$': '<rootDir>/src/mocks/cssMock.ts',
-		'\\/mocks/index$': '<rootDir>/src/mocks/mswMock.ts',
-	}
-}
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@lib(.*)$": "<rootDir>/src/lib$1",
+    "^@common(.*)$": "<rootDir>/src/common$1",
+    "^@mocks(.*)$": "<rootDir>/src/mocks$1",
+    "^@auth(.*)$": "<rootDir>/src/auth$1",
+    "\\.css$": "<rootDir>/src/mocks/cssMock.ts",
+    "\\/mocks/index$": "<rootDir>/src/mocks/mswMock.ts",
+  },
+  testEnvironment: "jsdom",
+};
