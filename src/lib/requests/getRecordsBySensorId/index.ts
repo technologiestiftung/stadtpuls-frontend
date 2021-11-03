c794a4c045cf8275aaf8f6104ac76e98c0d4be53
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-11-03 17:24:20 +0100

    fix(STADTPULS-438): provide count with every select from records

--- src/lib/requests/getRecordsBySensorId/index.ts
+++ src/lib/requests/getRecordsBySensorId/index.ts
@@ -4,75 +4,109 @@ import { definitions } from "@common/types/supabase";
 export interface GetRecordsOptionsType {
   startDate?: string;
   endDate?: string;
+  maxRows?: number;
+}
+
+export interface GetRecordsResponseType {
+  records: definitions["records"][];
+  count: number | null;
 }
 
 export const getRecordsBySensorId = async (
   sensorId: number,
   options?: GetRecordsOptionsType
-): Promise<definitions["records"][]> => {
+): Promise<GetRecordsResponseType> => {
+  const MAX_ROWS =
+    options?.maxRows ||
+    Number.parseInt(process.env.NEXT_PUBLIC_SUPABASE_MAX_ROWS as string, 10);
+
   if (options) {
     switch (true) {
       case !!(options && options.startDate && options.endDate): {
-        const { data: records, error } = await supabase
+        const {
+          data: records,
+          error,
+          count,
+        } = await supabase
           .from<definitions["records"]>("records")
-          .select("*")
+          .select("*", { count: "exact", head: false })
+          .limit(MAX_ROWS)
           .eq("sensor_id", sensorId)
           .gte("recorded_at", options.startDate)
           .lte("recorded_at", options.endDate)
           .order("recorded_at", { ascending: false });
         if (error) throw error;
         if (!records) throw new Error(`No records found for this time range`);
-
-        return records;
+        return { records, count };
       }
 
       case !!(options && options.startDate && !options.endDate): {
-        const { data: records, error } = await supabase
+        const {
+          data: records,
+          error,
+          count,
+        } = await supabase
           .from<definitions["records"]>("records")
-          .select("*")
+          .select("*", { count: "exact" })
+          .limit(MAX_ROWS)
           .eq("sensor_id", sensorId)
           .gte("recorded_at", options.startDate)
           .order("recorded_at", { ascending: false });
 
         if (error) throw error;
         if (!records) throw new Error(`No records found for this time range`);
-        return records;
+        return { records, count };
       }
 
       case !!(options && !options.startDate && options.endDate): {
-        const { data: records, error } = await supabase
+        const {
+          data: records,
+          error,
+          count,
+        } = await supabase
           .from<definitions["records"]>("records")
-          .select("*")
+          .select("*", { count: "exact" })
+          .limit(MAX_ROWS)
           .eq("sensor_id", sensorId)
           .lte("recorded_at", options.endDate)
           .order("recorded_at", { ascending: false });
 
         if (error) throw error;
         if (!records) throw new Error(`No records found for this time range`);
-        return records;
+        return { records, count };
       }
 
       default: {
-        const { data: records, error } = await supabase
+        const {
+          data: records,
+          error,
+          count,
+        } = await supabase
           .from<definitions["records"]>("records")
-          .select("*")
+          .select("*", { count: "exact" })
+          .limit(MAX_ROWS)
           .eq("sensor_id", sensorId)
           .order("recorded_at", { ascending: false });
 
         if (error) throw error;
         if (!records) throw new Error(`No records found`);
-        return records;
+        return { records, count };
       }
     }
   } else {
-    const { data: records, error } = await supabase
+    const {
+      data: records,
+      error,
+      count,
+    } = await supabase
       .from<definitions["records"]>("records")
-      .select("*")
+      .select("*", { count: "exact" })
+      .limit(MAX_ROWS)
       .eq("sensor_id", sensorId)
       .order("recorded_at", { ascending: false });
 
     if (error) throw error;
     if (!records) throw new Error(`No records found for sensor ID ${sensorId}`);
-    return records;
+    return { records, count };
   }
 };
