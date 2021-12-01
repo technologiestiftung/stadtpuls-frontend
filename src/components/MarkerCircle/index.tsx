1b9be06c82e7b90272a45a4a6e21aab5cd0aa66a
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-12-01 17:46:58 +0100

    style(MarkerCircle): fix positioning of circle

--- src/components/MarkerCircle/index.tsx
+++ src/components/MarkerCircle/index.tsx
@@ -6,13 +6,14 @@ export const MarkerCircle: FC<{
 }> = ({ isActive, clickHandler, children }) => {
   return (
     <div className='w-6 h-6 flex'>
-      <div className='motion-safe:animate-ping absolute inline-flex h-full w-full rounded-full bg-green opacity-90'></div>
+      <div className='motion-safe:animate-ping absolute -top-3 -left-3 inline-flex h-full w-full rounded-full bg-green opacity-90'></div>
       <button
         className={[
           "relative inline-flex",
           "w-6 h-6 rounded-full",
           `${isActive ? "bg-blue" : "bg-gray-500"} border-white border-2`,
           "text-center",
+          "-translate-x-3 -translate-y-3",
           `${clickHandler ? "cursor-pointer" : "cursor-default"}`,
         ].join(" ")}
         onClick={() => (clickHandler ? clickHandler() : null)}
