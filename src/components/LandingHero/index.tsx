8346fb6cba08420c4ebfdb66f74773e7f36418ca
Ingo Hinterding <ingo@hinterding.de>
2021-12-02 12:17:22 +0100

    update text

--- src/components/LandingHero/index.tsx
+++ src/components/LandingHero/index.tsx
@@ -26,13 +26,13 @@ export const LandingHero: FC = () => (
               "mt-6 mb-1",
             ].join(" ")}
           >
-            Deine Sensoren erklären die (Um)welt?
+            Deine Sensoren erklären Berlin!
           </h1>
           <h2
             className='animate-slide-in text-white text-lg font-sans font-normal mb-4'
             style={{ animationDelay: "50ms" }}
           >
-            Teile die Daten und dein Wissen!
+            Teile deine Daten und dein Wissen für eine smarte Stadt
           </h2>
           <p
             className='animate-slide-in mb-8 max-w-prose'
@@ -40,31 +40,34 @@ export const LandingHero: FC = () => (
           >
             Stadtpuls ist die{" "}
             <strong className='text-green'>
-              offene Datenplattform für IoT-Projekte
-            </strong>{" "}
-            deiner Stadt. Und das beste: jede*r kann mitmachen. Egal ob du
-            offene Daten suchst oder bereits eigene Sensoren und Daten hast:
-            Stadtpuls hilft dir, tiefer in die Welt der Stadt-Sensorik
-            einzutauchen und deine Stadt besser zu verstehen.
+              offene Datenplattform für IoT-Projekte in Berlin
+            </strong>
+            .
+            <br />
+            Jede:r kann mitmachen, egal ob du offene Daten suchst oder bereits
+            eigene Sensoren und Daten hast, die du teilen möchtest. Stadtpuls
+            hilft dir, tiefer in die Welt der Echtzeit-Sensorik einzutauchen und
+            deine Stadt besser zu verstehen.
           </p>
-          <Link href='/sensors'>
-            <AnchorButton
-              href='/sensors'
-              variant='secondary'
-              className='animate-slide-in mr-4 mb-2'
-              style={{ animationDelay: "150ms" }}
-            >
-              Sensoren ansehen
-            </AnchorButton>
-          </Link>
+
           <Link href='/docs'>
             <AnchorButton
               href='/docs'
               variant='primary'
-              className='animate-slide-in'
+              className='animate-slide-in mr-4 mb-2'
               style={{ animationDelay: "200ms" }}
             >
-              Mitmachen
+              Jetzt mitmachen & Sensoren erstellen
+            </AnchorButton>
+          </Link>
+          <Link href='/sensors'>
+            <AnchorButton
+              href='/sensors'
+              variant='secondary'
+              className='animate-slide-in '
+              style={{ animationDelay: "150ms" }}
+            >
+              Sensoren ansehen
             </AnchorButton>
           </Link>
         </div>
