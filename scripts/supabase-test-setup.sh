f42348c6db6fcc53da72f80958b2d32fdfa8e0e3
ff6347 <fabianmoronzirfas@protonmail.ch>
2022-01-25 15:43:08 +0100

    Creates override file using heredoc
    also updates ignore files

--- scripts/supabase-test-setup.sh
+++ scripts/supabase-test-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-
+git config advice.detachedHead false
 git clone \
   --depth 1  \
   --filter=blob:none  \
@@ -10,3 +10,17 @@ git clone \
   https://github.com/technologiestiftung/stadtpuls-supabase.git supabase \
   && cd supabase \
   && git sparse-checkout set supabase-docker-compose
+
+cat >> ./supabase-docker-compose/docker-compose.override.yml << EOF
+
+services:
+  api:
+    image: technologiestiftung/stadtpuls-api:v3.0.0
+    ports:
+      - "4000:4000"
+  redis:
+    image: redis:6.2.6-alpine
+    ports:
+      - "6379:6379"
+EOF
+
