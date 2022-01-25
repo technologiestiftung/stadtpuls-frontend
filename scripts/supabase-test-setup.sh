10b9bcd7aa987770d168475a7cc153e74b170810
ff6347 <fabianmoronzirfas@protonmail.ch>
2022-01-25 15:21:07 +0100

    fix(setup): Only checkout needed files
    Also uses a specifc tag instead of targeting brnaches

---
+++ scripts/supabase-test-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+git clone \
+  --depth 1  \
+  --filter=blob:none  \
+  --sparse \
+  --branch v4.0.0 \
+  https://github.com/technologiestiftung/stadtpuls-supabase.git supabase \
+  && cd stadtpuls-supabase \
+  && git sparse-checkout set supabase-docker-compose
