27caa280c9b4ef1d3523edde4dce2d5b24460da7
Lucas Vogel <contact@vogelino.com>
2022-02-17 17:40:49 +0100

    fix(storybook): Fix path to snapshots

--- src/storybook.test.ts
+++ src/storybook.test.ts
@@ -1,18 +1,9 @@
 import initStoryshots, {
-  snapshotWithOptions,
+  multiSnapshotWithOptions,
 } from "@storybook/addon-storyshots";
 import path from "path";
 
 initStoryshots({
-  test: story => {
-    // This adjusts for the fact that the story path is jest config relative,
-    // but the storyshot write path is relative to where this file is.
-    const snapshotFileName = path.resolve(
-      __dirname,
-      "../..", // go backwards so when we go forwards again we end up in the right place.
-      story.stories2snapsConverter.getSnapshotFileName(story.context)
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return snapshotWithOptions()({ ...story, snapshotFileName });
-  },
+  integrityOptions: { cwd: path.resolve(__dirname, "../..") },
+  test: multiSnapshotWithOptions(),
 });
