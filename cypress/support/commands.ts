d47d9d52e21898c5597a0ae1180be06d806e84ac
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-04-07 12:40:30 +0200

    test(Cypress): add helpers for user management

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,62 @@
 import "@testing-library/cypress/add-commands";
+
+const supabaseUrl = Cypress.env("supabaseUrl");
+
+Cypress.Commands.add("signUp", () => {
+  cy.request({
+    method: "POST",
+    url: `${supabaseUrl}/auth/v1/signup`,
+    headers: {
+      apikey: Cypress.env("apikey"),
+    },
+    body: {
+      email: Cypress.env("email"),
+      password: Cypress.env("password"),
+    },
+    failOnStatusCode: true,
+  });
+});
+
+Cypress.Commands.add("signIn", () => {
+  cy.getSession().then(response => {
+    cy.window().then(win => {
+      win.localStorage.setItem(
+        "supabase.auth.token",
+        JSON.stringify({
+          currentSession: response.body,
+          expiresAt: response.body.expires_at,
+        })
+      );
+    });
+  });
+
+  cy.visit("/");
+});
+
+Cypress.Commands.add("getSession", () => {
+  return cy.request({
+    // TODO: why does only v1 work? Should be v3
+    method: "POST",
+    url: `${supabaseUrl}/auth/v1/token?grant_type=password`,
+    headers: {
+      apikey: Cypress.env("apikey"),
+    },
+    body: {
+      email: Cypress.env("email"),
+      password: Cypress.env("password"),
+    },
+    failOnStatusCode: true,
+  });
+});
+
+Cypress.Commands.add("deleteUser", token => {
+  cy.request({
+    method: "POST",
+    url: `${supabaseUrl}/rest/v1/rpc/delete_user`,
+    headers: {
+      apikey: Cypress.env("apikey"),
+      Authorization: `Bearer ${token}`,
+    },
+    failOnStatusCode: true,
+  });
+});
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,14 +1,59 @@
+// load type definitions that come with Cypress module
+/// <reference types="cypress" />
 import "./commands";
-import inViewport from './inViewport'
+import inViewport from "./inViewport";
+
+export interface CurrentSessionType {
+  access_token: string;
+  [key: string]: unknown;
+}
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Signs up the test user defined in cypress.json
+       * @example cy.signUp()
+       */
+      signUp(): Chainable<Element>;
+
+      /**
+       * Deletes the user of the corresponding token
+       * @example cy.deleteUser()
+       */
+      deleteUser(token: string): Chainable<Element>;
+
+      /**
+       * Signs in the test user by saving the session from getSession to localStorage
+       * @example cy.signUp()
+       */
+      signIn(): Chainable<Element>;
+
+      /**
+       * Retrieves a session with token etc.
+       * @example cy.getSession()
+       */
+      getSession(): Chainable<
+        Cypress.Response<{
+          body: {
+            currentSession: CurrentSessionType;
+            expiresAt: number;
+          };
+          [key: string]: unknown;
+        }>
+      >;
+    }
+  }
+}
 
 before(() => {
-    chai.use(inViewport);
+  chai.use(inViewport);
 });
 
-const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
-Cypress.on('uncaught:exception', (err) => {
-    /* returning false here prevents Cypress from failing the test */
-    if (resizeObserverLoopErrRe.test(err.message)) {
-        return false
-    }
-})
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+Cypress.on("uncaught:exception", err => {
+  /* returning false here prevents Cypress from failing the test */
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    return false;
+  }
+});
