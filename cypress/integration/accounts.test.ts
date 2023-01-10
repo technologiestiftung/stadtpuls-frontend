3e83289264ef9c07d5df2ea2c7ebd962f93ef5dc
Lucas Vogel <contact@vogelino.com>
2023-01-10 12:01:29 +0100

    ci(cypress): Remove integration tests

--- cypress/integration/accounts.test.ts
+++
@@ -1,7 +0,0 @@
-describe("The Accounts page", () => {
-  it("successfully loads", () => {
-    cy.visit("/accounts");
-
-    cy.findByRole("heading", { name: /Alle Accounts/i }).should("exist");
-  });
-});
--- cypress/integration/docs.test.ts
+++
@@ -1,9 +0,0 @@
-describe("The Docs page", () => {
-  it("successfully loads", () => {
-    cy.visit("/docs");
-
-    cy.findByRole("heading", { name: /Willkommen bei Stadtpuls/i }).should(
-      "exist"
-    );
-  });
-});
